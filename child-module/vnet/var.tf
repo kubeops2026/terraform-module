640f0573c0783cdb516c7d35f12e89d0ad0862c9
kubeops2026 <kubeops2026@gmail.com>
2026-06-07 15:12:56 +0530

    Update var.tf

--- child-module/vnet/var.tf
+++ child-module/vnet/var.tf
@@ -1,14 +1,57 @@
-variable "vnet_var" {
-  description = "Map of VNets"
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "dns_servers" {
+  type    = list(string)
+  default = null
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "ddos_protection_plan_id" {
+  type    = string
+  default = null
+}
+
+variable "subnets" {
 
   type = map(object({
-    name                = string
-    location            = string
-    resource_group_name = string
-    address_space       = list(string)
 
-    dns_servers             = optional(list(string), [])
-    flow_timeout_in_minutes = optional(number)
-    tags                    = optional(map(string), {})
+    address_prefixes = list(string)
+
+    service_endpoints = optional(list(string))
+
+    private_endpoint_network_policies = optional(string)
+
+    private_link_service_network_policies_enabled = optional(bool)
+
+    nsg_id = optional(string)
+
+    delegation = optional(object({
+      name = string
+
+      service_delegation = object({
+        name    = string
+        actions = list(string)
+      })
+    }))
   }))
+
+  default = {}
 }
