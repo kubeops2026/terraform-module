e710ae96c4567e1f236fa97568895594afa97ee0
kubeops2026 <kubeops2026@gmail.com>
2026-06-07 15:25:30 +0530

    Delete parent-module/vnet directory

--- parent-module/vnet/output.tf
+++
@@ -1,7 +0,0 @@
-output "vnet_id" {
-  value = module.vnet.vnet_id
-}
-
-output "subnet_ids" {
-  value = module.vnet.subnet_ids
-}
--- parent-module/vnet/var.tf
+++
@@ -1,6 +0,0 @@
-variable "rgvar-p" {
-    type = map(any)
-}
-variable "vnet_p"{
-    type = map(any)
-}
