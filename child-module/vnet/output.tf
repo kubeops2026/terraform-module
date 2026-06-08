237b0420681cbc4e9a166d871e710829e9434d7e
unknown <sumisharma90@gmail.com>
2026-06-08 22:25:13 +0530

    updated the modules

--- child-module/vnet/output.tf
+++ child-module/vnet/output.tf
@@ -1,16 +1,16 @@
 output "vnet_id" {
-  value = azurerm_virtual_network.this.id
+  value = azurerm_virtual_network.vnet.id
 }
 
 output "vnet_name" {
-  value = azurerm_virtual_network.this.name
+  value = azurerm_virtual_network.vnet.name
 }
 
 output "subnet_ids" {
 
   value = {
 
-    for k, v in azurerm_subnet.this :
+    for k, v in azurerm_subnet.subnet :
     k => v.id
   }
 }
