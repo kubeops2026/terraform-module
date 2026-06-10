49397e829f314b3d099832393a48bed0ba5c42fc
kubeops2026 <kubeops2026@gmail.com>
2026-06-10 22:25:07 +0530

    Update var.tf

--- parent-module/vnet/var.tf
+++ parent-module/vnet/var.tf
@@ -1,7 +1,38 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "vnet_name" {}
-variable "address_space" {}
-variable "dns_servers" {}
-variable "tags" {}
-variable "subnets" {}
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group where resources will be deployed."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be created."
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network."
+  type        = string
+}
+
+variable "address_space" {
+  description = "List of address spaces for the Virtual Network."
+  type        = list(string)
+}
+
+variable "dns_servers" {
+  description = "List of DNS server IP addresses for the Virtual Network."
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "Map of tags to assign to resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "subnets" {
+  description = "Map of subnet configurations."
+  type = map(object({
+    address_prefixes = list(string)
+  }))
+}
